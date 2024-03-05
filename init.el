72559e78d556b55ee9087726f2e50ca34887d155
youguanxinqing <youguanxinqing@qq.com>
2024-03-05 23:45:01 +0800

    add marginalia, company, embark

--- init.el
+++ init.el
@@ -4,6 +4,9 @@
                          ("nongnu" . "https://mirrors.ustc.edu.cn/elpa/nongnu/")))
 (package-initialize)
 
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
 ;; --- begin add site-lisp ---
 (add-to-list 'load-path "~/.emacs.d/site-lisp/open-newline/")
 ;; --- end 
@@ -17,7 +20,7 @@
  '(custom-safe-themes
    '("4b026ac68a1aa4d1a91879b64f54c2490b4ecad8b64de5b1865bca0addd053d9" "d92c1c36a5181cf629749bf6feee1886cf6bce248ab075c9d1b1f6096fea9539" "98ef36d4487bf5e816f89b1b1240d45755ec382c7029302f36ca6626faf44bbd" "ba323a013c25b355eb9a0550541573d535831c557674c8d59b9ac6aa720c21d3" "046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" "bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "21e3d55141186651571241c2ba3c665979d1e886f53b2e52411e9e96659132d4" default))
  '(package-selected-packages
-   '(consult wgrep embark-consult which-key evil rg orderless doom-modeline color-theme org-roam-ui org-roam vertico modus-themes))
+   '(embark marginalia company consult wgrep embark-consult which-key evil rg orderless doom-modeline color-theme org-roam-ui org-roam vertico modus-themes))
  '(warning-suppress-log-types '((emacs)))
  '(warning-suppress-types '((emacs))))
 (custom-set-faces
@@ -41,9 +44,6 @@
 (set-fontset-font "fontset-default" 'han "微软雅黑" nil 'append)
 (load-theme 'modus-operandi-tinted t)
 (tool-bar-mode 0)
-
-;; keymap
-(global-set-key (kbd "C-h") 'delete-backward-char)
 
 ;; --- end 
 
@@ -91,6 +91,18 @@
 
 (require 'which-key)
 (which-key-mode)
+
+(require 'marginalia)
+(marginalia-mode t)
+
+(global-company-mode 1)
+;; company mode 默认选择上一条和下一条候选项命令 M-n M-p
+(define-key company-active-map (kbd "C-n") 'company-select-next)
+(define-key company-active-map (kbd "C-p") 'company-select-previous)
+
+(package-install 'embark)
+(global-set-key (kbd "C-;") 'embark-act)
+(setq prefix-help-command 'embark-prefix-help-comman)
 
 (package-install 'consult)
 ;;replace swiper
