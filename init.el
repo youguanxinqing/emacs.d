d70259c448a968dbf1a8b97f50a164a9e96eac32
youguanxinqing <youguanxinqing@qq.com>
2024-03-07 23:36:45 +0800

    improve search

--- init.el
+++ init.el
@@ -20,7 +20,7 @@
  '(custom-safe-themes
    '("4b026ac68a1aa4d1a91879b64f54c2490b4ecad8b64de5b1865bca0addd053d9" "d92c1c36a5181cf629749bf6feee1886cf6bce248ab075c9d1b1f6096fea9539" "98ef36d4487bf5e816f89b1b1240d45755ec382c7029302f36ca6626faf44bbd" "ba323a013c25b355eb9a0550541573d535831c557674c8d59b9ac6aa720c21d3" "046a2b81d13afddae309930ef85d458c4f5d278a69448e5a5261a5c78598e012" "7b8f5bbdc7c316ee62f271acf6bcd0e0b8a272fdffe908f8c920b0ba34871d98" "d445c7b530713eac282ecdeea07a8fa59692c83045bf84dd112dd738c7bcad1d" "bbb13492a15c3258f29c21d251da1e62f1abb8bbd492386a673dcfab474186af" "7fd8b914e340283c189980cd1883dbdef67080ad1a3a9cc3df864ca53bdc89cf" "21e3d55141186651571241c2ba3c665979d1e886f53b2e52411e9e96659132d4" default))
  '(package-selected-packages
-   '(embark marginalia company consult wgrep embark-consult which-key evil rg orderless doom-modeline color-theme org-roam-ui org-roam vertico modus-themes))
+   '(pyim embark marginalia company consult wgrep embark-consult which-key evil rg orderless doom-modeline color-theme org-roam-ui org-roam vertico modus-themes))
  '(warning-suppress-log-types '((emacs)))
  '(warning-suppress-types '((emacs))))
 (custom-set-faces
@@ -41,10 +41,14 @@
 (setq-default buffer-file-coding-system 'utf-8)
 (setq-default default-buffer-file-coding-system 'utf-8)
 
-(set-frame-font "Courier New 14" nil t)
+(set-frame-font "Courier New 13" nil t)
 (set-fontset-font "fontset-default" 'han "微软雅黑" nil 'append)
 (load-theme 'modus-operandi-tinted t)
 (tool-bar-mode 0)
+(global-display-line-numbers-mode t)
+;;让鼠标滚动更好用
+(setq mouse-wheel-scroll-amount '(1 ((shift) . 1) ((control) . nil)))
+(setq mouse-wheel-progressive-speed nil)
 
 (global-set-key (kbd "C-h C-f") 'find-function)
 (global-set-key (kbd "C-h C-v") 'find-variable)
@@ -119,6 +123,26 @@
 ;;consult-imenu
 (global-set-key (kbd "C-x b") 'consult-buffer)
 
+(defun consult-directory-externally (file)
+  "Open FILE externally using the default application of the system."
+  (interactive "fOpen externally: ")
+  (if (and (eq system-type 'windows-nt)
+           (fboundp 'w32-shell-execute))
+      (shell-command-to-string (encode-coding-string (replace-regexp-in-string "/" "\\\\"
+                                                                               (format "explorer.exe %s" (file-name-directory (expand-file-name file)))) 'gbk))
+    (call-process (pcase system-type
+                    ('darwin "open")
+                    ('cygwin "cygstart")
+                    (_ "xdg-open"))
+                  nil 0 nil
+                  (file-name-directory (expand-file-name file)))))
+
+(define-key embark-file-map (kbd "E") #'consult-directory-externally)
+;;打开当前文件的目录
+(defun my-open-current-directory ()
+  (interactive)
+  (consult-directory-externally default-directory))
+
 (package-install 'embark-consult)
 (package-install 'wgrep)
 (setq wgrep-auto-save-buffer t)
@@ -155,6 +179,23 @@
      consult-async-input-throttle 0.2
      consult-async-input-debounce 0.1)
     ))
+
+
+(package-install 'pyim)
+(defun eh-orderless-regexp (orig_func component)
+  (let ((result (funcall orig_func component)))
+    (pyim-cregexp-build result)))
+(defun toggle-chinese-search ()
+  (interactive)
+  (if (not (advice-member-p #'eh-orderless-regexp 'orderless-regexp))
+      (advice-add 'orderless-regexp :around #'eh-orderless-regexp)
+    (advice-remove 'orderless-regexp #'eh-orderless-regexp)))
+(defun disable-py-search (&optional args)
+  (if (advice-member-p #'eh-orderless-regexp 'orderless-regexp)
+      (advice-remove 'orderless-regexp #'eh-orderless-regexp)))
+;; (advice-add 'exit-minibuffer :after #'disable-py-search)
+(add-hook 'minibuffer-exit-hook 'disable-py-search)
+(global-set-key (kbd "s-q") 'toggle-chinese-search)
 
 
 ;; --- begin site-lisp
