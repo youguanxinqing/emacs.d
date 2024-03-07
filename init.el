59c4919094c8b460c71a8f6ec630f0488cdb9699
youguanxinqing <youguanxinqing@qq.com>
2024-03-07 08:45:27 +0800

    add emacs

--- init.el
+++ init.el
@@ -46,6 +46,10 @@
 (load-theme 'modus-operandi-tinted t)
 (tool-bar-mode 0)
 
+(global-set-key (kbd "C-h C-f") 'find-function)
+(global-set-key (kbd "C-h C-v") 'find-variable)
+(global-set-key (kbd "C-h C-k") 'find-function-on-key)
+
 ;; --- end 
 
 (use-package vertico
@@ -158,3 +162,27 @@
 (global-set-key (kbd "C-o") 'open-newline-below)
 (global-set-key (kbd "C-S-o") 'open-newline-above)
 ;; --- end 
+
+
+(progn
+  (defvar emax-root (concat (expand-file-name "~") "/emax"))
+  (defvar emax-bin (concat emax-root "/bin"))
+  (defvar emax-bin64 (concat emax-root "/bin64"))
+
+  (setq exec-path (cons emax-bin exec-path))
+  (setenv "PATH" (concat emax-bin ";" (getenv "PATH")))
+
+  (setq exec-path (cons emax-bin64 exec-path))
+  (setenv "PATH" (concat emax-bin64 ";" (getenv "PATH")))
+
+  (setq emacsd-bin (concat user-emacs-directory "bin"))
+  (setq exec-path (cons  emacsd-bin exec-path))
+  (setenv "PATH" (concat emacsd-bin  ";" (getenv "PATH")))
+
+  ;;可选安装msys64
+  ;;下载地址: http://repo.msys2.org/mingw/sources/
+  ;; (setenv "PATH" (concat "C:\\msys64\\usr\\bin;C:\\msys64\\mingw64\\bin;" (getenv "PATH")))
+
+  ;; (dolist (dir '("~/emax/" "~/emax/bin/" "~/emax/bin64/" "~/emax/lisp/" "~/emax/elpa/"))
+  ;;   (add-to-list 'load-path dir))
+  )
