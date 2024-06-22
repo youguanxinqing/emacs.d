5b4ab77dafcf479d41c7da7edfcc7f1e69c4361d
youguanxinqing <youguanxinqing@qq.com>
2024-06-23 00:18:09 +0800

    add rime

--- packages.el
+++ packages.el
@@ -48,3 +48,9 @@
 ;; (unpin! pinned-package another-pinned-package)
 ;; ...Or *all* packages (NOT RECOMMENDED; will likely break things)
 ;; (unpin! t)
+
+(package! rime
+  :recipe (:host github
+           :repo "DogLooksGood/emacs-rime"
+           :files ("*.el" "Makefile" "lib.c"))
+)
