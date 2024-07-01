34d4a0593e638bed9ab975f1fe36a4601431dca5
youguanxinqing <youguanxinqing@qq.com>
2024-07-02 00:18:29 +0800

    refactor code

--- config.el
+++ config.el
@@ -30,47 +30,6 @@
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
 ;;
 
-;; ----------- customize configuration start ---
-
-(add-to-list 'load-path "~/.config/doom/customizations")
-(load "init.el")
-
-;; in $DOOMDIR/config.el
-(setq rustic-lsp-server 'rust-analyzer)
-
-;; close evil-snipe-s
-(evil-define-key '(normal motion) evil-snipe-local-mode-map
-  "s" nil
-  "S" nil)
-
-;; rime
-(use-package! rime
-  :config
-  (setq default-input-method "rime"
-        rime-show-candidate 'posframe))
-(global-set-key (kbd "C-x C-d") 'toggle-input-method)
-(global-set-key (kbd "M-j") 'rime-inline-ascii)
-
-(global-set-key (kbd "C-s") 'evil-avy-goto-word-1)
-
-(map! :n "C-t" nil)
-(define-key evil-motion-state-map (kbd "C-t") 'evil-jump-backward-swap)
-(global-set-key [remap evil-jump-to-tag] nil)
-(define-key evil-motion-state-map (kbd "C-]") 'evil-jump-to-tag)
-
-(map! :leader
-
-      :desc "Find file from here"  "SPC"  #'find-file
-
-      (:prefix-map ("f" . "file")
-       :desc "Search project"  "w"  #'+default/search-project
-       :desc "Find file from workspace"  "f"  #'+default/find-file-under-here
-       :desc "Find file from here"  "F"  #'find-file
-       :desc "Search current directory"  "d"  #'+default/search-cwd)
-      )
-
-;; ----------- customize configuration end   ---
-
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -120,3 +79,11 @@
 
 
 ;; (setq doom-font (font-spec :family "Jetbrains Mono" :size 19))
+
+
+;; ----------- customize configuration start ---
+
+(add-to-list 'load-path "~/.config/doom/customizations")
+(load "init.el")
+
+;; ----------- customize configuration end   ---
--- customizations/init.el
+++ customizations/init.el
@@ -1,7 +1,41 @@
 ;;; customizations/init.el -*- lexical-binding: t; -*-
 
 (setq doom-font (font-spec :size 22))
+(setq rustic-lsp-server 'rust-analyzer)
 
+;; close evil-snipe-s
+(evil-define-key '(normal motion) evil-snipe-local-mode-map
+  "s" nil
+  "S" nil)
+
+;; rime
+(use-package! rime
+  :config
+  (setq default-input-method "rime"
+        rime-show-candidate 'posframe))
+(global-set-key (kbd "C-x C-d") 'toggle-input-method)
+(global-set-key (kbd "M-j") 'rime-inline-ascii)
+
+(global-set-key (kbd "C-s") 'evil-avy-goto-word-1)
+
+(map! :n "C-t" nil)
+(define-key evil-motion-state-map (kbd "C-t") 'evil-jump-backward-swap)
+(global-set-key [remap evil-jump-to-tag] nil)
+(define-key evil-motion-state-map (kbd "C-]") 'evil-jump-to-tag)
+
+(map! :leader
+
+      :desc "Find file from here"  "SPC"  #'find-file
+
+      (:prefix-map ("f" . "file")
+       :desc "Search project"  "w"  #'+default/search-project
+       :desc "Find file from workspace"  "f"  #'+default/find-file-under-here
+       :desc "Find file from here"  "F"  #'find-file
+       :desc "Search current directory"  "d"  #'+default/search-cwd)
+      )
+
+
+;; high priority config
 (setq private-custom-file "~/.config/doom/private/custom.el")
 (if (file-exists-p private-custom-file)
     (load private-custom-file) nil)
