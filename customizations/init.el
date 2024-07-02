19c8faadd89fcf491e85ca3e847a269acf95d484
youguanxinqing <youguanxinqing@qq.com>
2024-07-03 00:07:40 +0800

    optimize chinese font

--- customizations/init.el
+++ customizations/init.el
@@ -1,6 +1,57 @@
 ;;; customizations/init.el -*- lexical-binding: t; -*-
 
+;; font --------- start
 (setq doom-font (font-spec :size 22))
+
+(defun guan/--font-installed-p (font-name)
+  "Check if font with FONT-NAME is available."
+  (find-font (font-spec :name font-name)))
+
+(defun guan/enable-auto-set-font ()
+  (when (display-graphic-p)
+   (cl-loop for font in '("Cascadia Code" "SF Mono" "Source Code Pro"
+                           "Fira Code" "Menlo" "Monaco" "Dejavu Sans Mono"
+                           "Lucida Console" "Consolas" "SAS Monospace")
+           when (guan/--font-installed-p font)
+           return (set-face-attribute
+                   'default nil
+                   :font (font-spec :family font
+                                   :weight 'normal
+                                   :slant 'normal
+                                   :size (cond ((eq system-type 'gnu/linux) 14.0)
+                                                   ((eq system-type 'windows-nt) 12.5)))))
+   (cl-loop for font in '("OpenSansEmoji" "Noto Color Emoji" "Segoe UI Emoji"
+                           "EmojiOne Color" "Apple Color Emoji" "Symbola" "Symbol")
+           when (guan/--font-installed-p font)
+           return (set-fontset-font t 'unicode
+                                   (font-spec :family font
+                                           :size (cond ((eq system-type 'gnu/linux) 16.5)
+                                                           ((eq system-type 'windows-nt) 15.0)))
+                                   nil 'prepend))
+   (cl-loop for font in '("noto-fonts-cjk" "思源黑体 CN" "思源宋体 CN" "微软雅黑 CN"
+                           "Source Han Sans CN" "Source Han Serif CN"
+                           "WenQuanYi Micro Hei" "文泉驿等宽微米黑"
+                           "Microsoft Yahei UI" "Microsoft Yahei")
+           when (guan/--font-installed-p font)
+           return (set-fontset-font t '(#x4e00 . #x9fff)
+                                   (font-spec :name font
+                                           :weight 'normal
+                                           :slant 'normal
+                                           :size (cond ((eq system-type 'gnu/linux) 16.5)
+                                                           ((eq system-type 'windows-nt) 15.0)))))
+   (cl-loop for font in '("HanaMinB" "SimSun-ExtB")
+        when (guan/--font-installed-p font)
+        return (set-fontset-font t '(#x20000 . #x2A6DF)
+                                (font-spec :name font
+                                        :weight 'normal
+                                        :slant 'normal
+                                        :size (cond ((eq system-type 'gnu/linux) 16.5)
+                                                        ((eq system-type 'windows-nt) 15.0))))))
+        )
+(guan/enable-auto-set-font)
+
+;; font --------- end
+
 (setq rustic-lsp-server 'rust-analyzer)
 
 
