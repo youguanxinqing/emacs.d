6ed94675d45eb174758a3cc8aaeaf817c0f58ad6
youguanxinqing <youguanxinqing@qq.com>
2024-07-02 23:56:59 +0800

    optimize rime input method

--- customizations/init.el
+++ customizations/init.el
@@ -3,20 +3,45 @@
 (setq doom-font (font-spec :size 22))
 (setq rustic-lsp-server 'rust-analyzer)
 
+
+;; rime ----------- start
+
+(defun guan/--get-input-method (action)
+  (cond ((string-equal action "switch") (if (rime--ascii-mode-p)
+                                            "Rime: To English"
+                                            "Rime: To Chinese"))
+        ((string-equal action "show") (if (rime--ascii-mode-p)
+                                          "Rime: English"
+                                          "Rime: Chinese"))
+        ))
+
+(defun guan/switch-input-method ()
+  "switch input method: english or chinese"
+  (interactive)
+  (rime-inline-ascii)
+  (rime--message-display-content (guan/--get-input-method "switch")))
+
+(defun guan/show-input-method ()
+  "show rime current input method"
+  (interactive)
+  (rime--message-display-content (guan/--get-input-method "show")))
+
+(use-package! rime
+  :config
+  (setq default-input-method "rime"
+        rime-show-candidate 'posframe)
+  (global-set-key (kbd "C-x C-d") 'toggle-input-method)
+  (global-set-key (kbd "M-j") 'guan/switch-input-method))
+
+;; rime ----------- end
+
+(global-set-key (kbd "C-s") 'evil-avy-goto-word-1)
+
+
 ;; close evil-snipe-s
 (evil-define-key '(normal motion) evil-snipe-local-mode-map
   "s" nil
   "S" nil)
-
-;; rime
-(use-package! rime
-  :config
-  (setq default-input-method "rime"
-        rime-show-candidate 'posframe))
-(global-set-key (kbd "C-x C-d") 'toggle-input-method)
-(global-set-key (kbd "M-j") 'rime-inline-ascii)
-
-(global-set-key (kbd "C-s") 'evil-avy-goto-word-1)
 
 (map! :n "C-t" nil)
 (define-key evil-motion-state-map (kbd "C-t") 'evil-jump-backward-swap)
