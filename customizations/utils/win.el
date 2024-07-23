b19098f8d5347ca2d2558da958a9ea577ad44ead
youguanxinqing <youguanxinqing@qq.com>
2024-07-23 22:56:32 +0800

    wrap text for display-content-to-window

--- customizations/utils/win.el
+++ customizations/utils/win.el
@@ -4,12 +4,13 @@
 
 ;;; Code:
 (defun guan/display-content-to-window (window-name title content)
-  "Display content in a window that named window-name."
+  "Display content in a window that named WINDOW-NAME."
   (switch-to-buffer-other-window window-name)
   (erase-buffer)
   (insert title)
   (insert "\n----\n\n")
   (insert content)
+  (fill-paragraph)
   )
 
 (provide 'win)
