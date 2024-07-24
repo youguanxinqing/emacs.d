ea54c1b45c6899568305b429866ec543b3e23720
youguanxinqing <youguanxinqing@qq.com>
2024-07-24 19:34:02 +0800

    fix duplicate window-name

--- customizations/utils/win.el
+++ customizations/utils/win.el
@@ -5,7 +5,9 @@
 ;;; Code:
 (defun guan/display-content-to-window (window-name title content)
   "Display content in a window that named WINDOW-NAME."
-  (switch-to-buffer-other-window window-name)
+  (if (not (string-equal window-name (buffer-name)))
+      (switch-to-buffer-other-window window-name)
+    nil)
   (erase-buffer)
   (insert title)
   (insert "\n----\n\n")
